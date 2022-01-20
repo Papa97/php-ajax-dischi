dcaa64bbaf425338ec5eae7c22dfcb451a92d627
Guido <ppapaguido@gmail.com>
2022-01-20 15:59:11 +0100

    problema con l'option di scelta del genere

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const app = new Vue ({
+    el: "#app",
+    data: {
+        albums: [],
+        sceltaGenere: "",
+    },
+    created() {
+        axios.get("./backend.php")
+        .then( (response) => {
+            this.albums = response.data;
+        })
+    },
+    methods: {
+        filtraGenere(payload){
+            this.sceltaGenere = payload;
+        },
+        // filtraGenere() {
+        //     if (this.sceltaGenere == "tutti") {
+        //         return this.album;
+        //     } else {
+        //         return this.album.filter((elm) => elm.genre.includes(this.sceltaGenere));
+        //     };
+        // }
+    },
+    computed: {
+        genereFiltered () {
+            if (this.sceltaGenere == "tutti") {
+                return this.albums;
+            } else {
+                return this.albums.filter((elm) => 
+                elm.genre.includes(this.sceltaGenere)
+                );
+            }
+        }
+    }
+})
